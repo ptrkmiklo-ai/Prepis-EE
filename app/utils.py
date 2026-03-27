e2cbc65d40190869ca4edc6389c797dc681e9df1
ptrkmiklo-ai <ptrkmiklo@gmail.com>
2026-03-27 13:50:43 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import os
 TEMPLATE_DIR = "/app/templates"
 
 def get_template_path():
-    path = os.path.join(TEMPLATE_DIR, "zse.pdf")
+    path = os.path.join(TEMPLATE_DIR, "ZSE.pdf")
     if not os.path.exists(path):
         raise FileNotFoundError(f"Šablóna ZSE neexistuje: {path}")
     return path
