5205fe3664d094219f527593e2d1f7a58fd68471
ptrkmiklo-ai <ptrkmiklo@gmail.com>
2026-03-31 10:42:48 +0200

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def load_adresar():
-    csv_path = "/data/in/tables/ADRESAR.csv"   # EXACT NAME FROM KEEBOOLA
+    csv_path = "/data/in/tables/ADRESAR.csv"
     if not os.path.exists(csv_path):
         raise FileNotFoundError(f"adresar.csv neexistuje: {csv_path}")
     return pd.read_csv(csv_path).fillna("")
